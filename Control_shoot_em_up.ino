7d23041778c4a06c1d85c7bad1d054bea9a53a2c
chaos20 <33138600+chaos20@users.noreply.github.com>
2017-11-15 18:07:36 -0600

    Add files via upload

---
+++ Control_shoot_em_up.ino
@@ -0,0 +1,19 @@
+const int SWITCH = 2;
+const int x = 1;
+const int y = 0;
+const int a = 7;
+
+void setup(){
+  pinMode(SWITCH,INPUT);
+  digitalWrite(SWITCH,HIGH);
+
+  pinMode(a,INPUT);
+  digitalWrite(a,HIGH);
+  
+  Serial.begin(9600);
+ 
+}
+void loop() {
+  Serial.println("{\"x\" :" + (String)analogRead(x) + ", \"y\":" + (String)analogRead(y)+ ", \"a\":" + (String)digitalRead(a) +"}");
+
+}
